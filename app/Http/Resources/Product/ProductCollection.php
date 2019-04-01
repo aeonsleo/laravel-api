ad1127aac6c20b280a974d84a7ed9596fdaa3916
Abhishek Srivastava <aeonsleo@gmail.com>
2019-04-02 02:12:50 +0530

    Product Collection resource used

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -13,7 +13,18 @@ class ProductCollection extends ResourceCollection
      * @return array
      */
     public function toArray($request)
-    {
-        return parent::toArray($request);
+    {   
+        
+        return [
+            'name' => $this->name,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No Rating Yet',
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+        ];
+        
+
+        // return parent::toArray($request);
     }
 }
